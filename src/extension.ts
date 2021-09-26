4bc23583c1f516f5adcdcd204b4907d05ce864f3
Abdallah-Moh <abdallah.mam29@gmail.com>
2021-09-26 13:53:24 +0200

    Basic command reading

--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,26 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
-
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
+import * as vscode from "vscode";
+import { exec } from "child_process";
+import G = require("glob");
 export function activate(context: vscode.ExtensionContext) {
-	
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "vs-valdo" is now active!');
+  let disposable = vscode.commands.registerCommand("vs-valdo.createNewValaProject", () => {
+    exec("valdo", (_err, stdout) => {
+      let parsedInput = stdout.split("\n");
+      parsedInput.shift();
+      parsedInput.shift();
+      parsedInput = parsedInput.filter((item) => {
+        return item !== "";
+      });
+      vscode.window.showQuickPick(parsedInput).then((template) => {
+        if (!template) {
+          return;
+        }
+        template = template.split("-")[0].replace(/ /g, "");
+        exec(`valdo ${template} --list-vars`, (_err, stdout) => {
+          const vars = JSON.parse(stdout);
+        });
+      });
+    });
+  });
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('vs-valdo.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from Vs Valdo!');
-	});
-
-	context.subscriptions.push(disposable);
+  context.subscriptions.push(disposable);
 }
-
-// this method is called when your extension is deactivated
-export function deactivate() {}
